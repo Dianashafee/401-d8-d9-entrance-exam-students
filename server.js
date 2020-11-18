99887650ca24aa4f3b7fd4d2267ddf37ea8b30e7
Dianashafee <diana.shafee1997@hotmail.com>
2020-11-18 12:23:38 +0200

    401exam

--- server.js
+++ server.js
@@ -43,19 +43,80 @@ app.set('view engine', 'ejs');
 // ----------------------
 // ------- Routes -------
 // ----------------------
+app.get('/home', handleHome);
+app.post('/savedChar', handlFact);
+app.get('/savedChar', handleSave);
+app.get('/savedChar/:id', handledDetails);
+app.put('/savedChar/:id', handledUpdate);
+app.delete('/savedChar/:id', handledDelete);
 
 
 // --------------------------------
 // ---- Pages Routes functions ----
 // --------------------------------
 
+function handleHome(req,res){
+let arr = [];
+let url = 'http://hp-api.herokuapp.com/api/characters';
+superagent.get(url).then(data =>{
+data.body.forEach(element => {
+    arr.push(new Potter(element));
+});
+res.render('home',{result: arr});
+});
+}
+
+function Potter(data){
+    this.image = data.image;
+    this.name = data.name;
+    this.house = data.house;
+    this.patronus = data.patronus;
+    this.alive = data.alive;
+}
 
 
+function handlFact(req, res){
+    let statment = 'INSERT INTO api (name,house,patronus,alive) VALUES ($1,$2,$3,$4);';
+    let val = [ req.body.name, req.body.house, req.body.patronus, req.body.alive];
+    client.query(statment, val).then(()=>{
+    res.redirect('/savedChar');
+    });
+}
+
+function handleSave(req,res){
+    let statment = 'SELECT * FROM api;';
+    client.query(statment).then(data =>{
+        res.render('saved',{result : data.rows});
+    });
+}
+
+
+function handledDetails (req,res){
+let statment = 'SELECT * FROM api WHERE id=$1;';
+let value = [req.params.id];
+client.query(statment,value).then(data=>{
+    res.render('details', {result: data.rows[0]});
+});
+}
 // -----------------------------------
 // --- CRUD Pages Routes functions ---
 // -----------------------------------
 
+function handledUpdate (req,res){
+    let statment = 'UPDATE api SET name=$1 ,patronus=$2, alive=$3 WHERE id=$4;';
+    let val = [req.body.name, req.body.patronus, req.body.alive, req.params.id];
+    client.query(statment, val).then(()=>{
+        res.redirect('/savedChar');
+    });
+}
 
+function handledDelete(req, res){
+    let statment = 'DELETE FROM api WHERE id=$1;';
+    let val = [req.params.id];
+    client.query(statment,val).then(()=>{
+        res.redirect('/savedChar');
+    })
+}
 
 // Express Runtime
 client.connect().then(() => {
